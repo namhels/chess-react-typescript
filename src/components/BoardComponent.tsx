1512a104bc882396af4cde57ddbe288bfee89f3a
namhels <8.lsv.888@gmail.com>
2023-05-11 23:10:47 +0300

    init app

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+export const BoardComponent = () => {
+  return (
+    <div className='board'>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+    </div>
+  )
+}
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CellComponent = () => {
+  return (
+    <div className='cell'></div>
+  )
+}
